2ac1ee1f045ac8552444ad5116fa1e827966ba22
cobpro <cobprogrammer@gmail.com>
2019-11-20 00:51:06 -0600

    Borrando archivos

--- biblebooks/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BiblebooksConfig(AppConfig):
-    name = 'biblebooks'
-    varbose_name = 'Libros de la Biblia'
--- biblebooks/static/js/SideBarSearch.js
+++
@@ -1,4 +0,0 @@
-/*
- * In this script are grouped the classes and functions related to the operation of the navbar component (id = "sidebar-search")
- */
-
